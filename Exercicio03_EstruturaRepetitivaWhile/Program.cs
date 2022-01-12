08afa43c25c6c3d113c6e0ad97a6bc873fb7bb5a
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2022-01-12 20:47:10 -0300

    First commit

---
+++ Exercicio03_EstruturaRepetitivaWhile/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03_EstruturaRepetitivaWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+
+            int tipo = int.Parse(Console.ReadLine());
+
+            while (tipo != 4)
+            {
+                if (tipo == 1)
+                {
+                    alcool++;
+                }
+                else if (tipo == 2)
+                {
+                    gasolina++;
+                }
+                else if (tipo == 3)
+                {
+                    diesel++;
+                }
+
+                tipo = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Muito Obrigado!");
+            Console.WriteLine("Álcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+        }
+    }
+}
